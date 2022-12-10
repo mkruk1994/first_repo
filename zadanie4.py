62ea5db62a999c7e51dfb5e18faf22b68db770f6
student-wroclaw <student-wroclaw@EDU-WSB.local>
2022-12-10 13:48:14 +0100

    zadanie4

---
+++ zadanie4.py
@@ -0,0 +1,9 @@
+#program otrzymuje liczbe 4ro cyfrowa i zwaraca sume cyfr
+
+x = input('podaj liczbę: ')
+z1 = int(x[0])
+z2 = int(x[1])
+z3 = int(x[2])
+z4 = int(x[3])
+suma = z1 + z2 + z3 + z4
+print(suma)
