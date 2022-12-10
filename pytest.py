014fa291fee84b779c5e2e804536c01b79e5985e
student-wroclaw <student-wroclaw@EDU-WSB.local>
2022-12-10 11:44:30 +0100

    odul

--- pytest.py
+++ pytest.py
@@ -1,1 +1,11 @@
-print ("sadsa")
+import os
+import time
+
+# print(os.getcwd()) #wyswietlenie obecnej lokalizacji #
+#os.chdir('C:\\Users\\vdi-student\\Desktop')
+print(os.getcwd())
+os.mkdir('new_folder') # tworzenie nowego foldera
+time.sleep(5)
+os.rename('new_folder', 'new_folder2') # zmiana nazwy
+time.sleep(5)
+os.rmdir('new_folder2')
