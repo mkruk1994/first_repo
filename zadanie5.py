da5db6c76c62c8f9007117ce8ff24e8c8ceb713e
student-wroclaw <student-wroclaw@EDU-WSB.local>
2022-12-10 14:53:25 +0100

    zadanie5

--- zadanie5.py
+++ zadanie5.py
@@ -6,5 +6,17 @@ with open(path, mode) as plik:
 for i in range (len(content)):
     content[i] = content[i].split(',')
 print(content)
-print(content[3][3])
+print(content[0][1])
+
+total = 0
+for i in range (1, len(content)):
+    total += int(content[i][1])
+average = total/(len(content) - 1)
+print(round(average,2))
+total = 0
+for i in range(1, len(content)):
+    if content[i][3] == 'k' and content[i][4] == 't':
+        total += +1
+print(total)
+
 
